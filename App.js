65bde167becfc9edcc926f3490904eeebe5af8cb
godswillokokon <godswillokokon3@gmail.com>
2019-12-12 13:45:58 +0100

    fixed icon issue

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { ApplicationProvider, Layout, Button } from '@ui-kitten/components';
-import { mapping, light as lightTheme } from '@eva-design/eva';
-import { default as appTheme } from './custom-theme.json'; // <-- Import app theme
+import { ApplicationProvider, Layout, Button, IconRegistry, Icon } from '@ui-kitten/components';
+import { mapping, dark as darkTheme } from '@eva-design/eva';
+import { default as appTheme } from './custom-theme.json';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-const theme = { ...lightTheme, ...appTheme };
-
+const theme = { ...darkTheme, ...appTheme };
+export const FacebookIcon = (style) => (
+  <Icon name='facebook' {...style} />
+);
 const HomeScreen = () => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Button>HOME</Button>
+    <Button icon={FacebookIcon}>HOME</Button>
   </Layout>
 );
 
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={theme}>
-    <HomeScreen />
-  </ApplicationProvider>
+  <React.Fragment>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={darkTheme}>
+      <HomeScreen />
+    </ApplicationProvider>
+  </React.Fragment>
 );
 
 export default App;
